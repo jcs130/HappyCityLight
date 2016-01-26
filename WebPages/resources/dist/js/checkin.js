61cd3529f23981682d1cbd80ee10b106c936acad
Zhongli Li <lzl19920403@gmail.com>
2016-01-25 23:16:44 -0500

    新增必要的文件

--- WebPages/resources/dist/js/checkin.js
+++ WebPages/resources/dist/js/checkin.js
@@ -21,83 +21,18 @@ function checkLogin() {
     user_lastname = localStorage.getItem("lastname");
     user_picture = localStorage.getItem("user_picture");
     if (logintoken == null || user_id == null) {
-         //通过localStorage获取登录信息
+        //通过localStorage获取登录信息
         logintoken = localStorage.getItem("token");
         user_id = localStorage.getItem("user_id");
-          //如果还是没有,则检查URL参数
+        //如果还是没有,则检查URL参数
         if (logintoken == null || user_id == null) {
-        //如果Cookie被禁用则通过URL获取tooken和userID然后向服务器请求其他参数
-        logintoken = getUrlParam("token");
-        user_id = getUrlParam("user_id");
+            //如果Cookie被禁用则通过URL获取tooken和userID然后向服务器请求其他参数
+            logintoken = getUrlParam("token");
+            user_id = getUrlParam("user_id");
             //alert("Cookie 被禁用,  token: " + logintoken + " <> user_id: " + user_id);
-        //如果参数获取不全则退回登陆界面
-        if (logintoken == null || user_id == null) {
-            //alert("参数不全");
-            //清除cookie
-            $.removeCookie('token', {
-                path: '/'
-            });
-            $.removeCookie('email', {
-                path: '/'
-            });
-            $.removeCookie('firstname', {
-                path: '/'
-            });
-            $.removeCookie('lastname', {
-                path: '/'
-            });
-            $.removeCookie('user_picture', {
-                path: '/'
-            });
-            $.removeCookie('user_id', {
-                path: '/'
-            });
-            window.location.href = "login.html?message=Please Login First.";
-        }
-        //连接服务器获取其他信息
-        $.ajax({
-            type: "GET",
-            crossDomain: true,
-            url: serverURL + "user/getuserdetail",
-            data: {
-                userID: user_id,
-                token: logintoken
-            },
-            dataType: "json",
-            success: function (data, textStatus) {
-                if (data.code == 200) {
-                    user_email = data.obj.email;
-                    user_picture = data.obj.user_picture;
-                    user_firstname = data.obj.firstname;
-                    user_lastname = data.obj.lastname;
-                    //在inside-link后面添加参数
-                    $("a.inside-link").each(function () {
-                        var _href = $(this).attr("href");
-                        $(this).attr("href", _href + "?user_id=" + user_id + "&token=" + logintoken);
-                        //alert($(this).attr("href"));
-                    });
-                    //更改页面上显示文字
-                    if (user_firstname != "null") {
-                        $(".userfirstname").html(user_firstname);
-                    } else {
-                        $(".userfirstname").html("");
-                    }
-                    if (user_lastname != "null") {
-                        $(".userlastname").html(user_lastname);
-                    } else {
-                        $(".userlastname").html("");
-                    }
-                    $(".useremail").html(user_email);
-                    checkin_afterChencin();
-                } else {
-                    alert(JSON.stringify(data));
-                    window.location.href = "login.html?message=Please Login Again."
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert(JSON.stringify(jqXHR));
-                // set [submit] button able if the submit is not
-                // successful
+            //如果参数获取不全则退回登陆界面
+            if (logintoken == null || user_id == null) {
+                //alert("参数不全");
                 //清除cookie
                 $.removeCookie('token', {
                     path: '/'
@@ -117,48 +52,114 @@ function checkLogin() {
                 $.removeCookie('user_id', {
                     path: '/'
                 });
+                window.location.href = "login.html?message=Please Login First.";
             }
-        });
+            //连接服务器获取其他信息
+            $.ajax({
+                type: "GET",
+                crossDomain: true,
+                url: serverURL + "user/getuserdetail",
+                data: {
+                    userID: user_id,
+                    token: logintoken
+                },
+                dataType: "json",
+                success: function (data, textStatus) {
+                    if (data.code == 200) {
+                        user_email = data.obj.email;
+                        user_picture = data.obj.user_picture;
+                        user_firstname = data.obj.firstname;
+                        user_lastname = data.obj.lastname;
+                        //在inside-link后面添加参数
+                        $("a.inside-link").each(function () {
+                            var _href = $(this).attr("href");
+                            $(this).attr("href", _href + "?user_id=" + user_id + "&token=" + logintoken);
+                            //alert($(this).attr("href"));
+                        });
+                        //更改页面上显示文字
+                        if (user_firstname != "null") {
+                            $(".userfirstname").html(user_firstname);
+                        } else {
+                            $(".userfirstname").html("");
+                        }
+                        if (user_lastname != "null") {
+                            $(".userlastname").html(user_lastname);
+                        } else {
+                            $(".userlastname").html("");
+                        }
+                        $(".useremail").html(user_email);
+                        checkin_afterChencin();
+                    } else {
+                        alert(JSON.stringify(data));
+                        window.location.href = "login.html?message=Please Login Again."
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(JSON.stringify(jqXHR));
+                    // set [submit] button able if the submit is not
+                    // successful
+                    //清除cookie
+                    $.removeCookie('token', {
+                        path: '/'
+                    });
+                    $.removeCookie('email', {
+                        path: '/'
+                    });
+                    $.removeCookie('firstname', {
+                        path: '/'
+                    });
+                    $.removeCookie('lastname', {
+                        path: '/'
+                    });
+                    $.removeCookie('user_picture', {
+                        path: '/'
+                    });
+                    $.removeCookie('user_id', {
+                        path: '/'
+                    });
+                }
+            });
         }
     }
-        //如果token过期则返回登陆界面
-        $.ajax({
-            type: "POST",
-            crossDomain: true,
-            url: serverURL + "user/tokencheck",
-            data: {
-                userID: user_id,
-                token: logintoken
-            },
-            dataType: "json",
-            success: function (data, textStatus) {
-                if (data.code == 200) {
-                    //token正确
-                    //更改页面上显示文字
-                    if (user_firstname != "null") {
-                        $(".userfirstname").html(user_firstname);
-                    } else {
-                        $(".userfirstname").html("");
-                    }
-                    if (user_lastname != "null") {
-                        $(".userlastname").html(user_lastname);
-                    } else {
-                        $(".userlastname").html("");
-                    }
-                    $(".useremail").html(user_email);
-                    checkin_afterChencin();
+    //如果token过期则返回登陆界面
+    $.ajax({
+        type: "POST",
+        crossDomain: true,
+        url: serverURL + "user/tokencheck",
+        data: {
+            userID: user_id,
+            token: logintoken
+        },
+        dataType: "json",
+        success: function (data, textStatus) {
+            if (data.code == 200) {
+                //token正确
+                //更改页面上显示文字
+                if (user_firstname != "null") {
+                    $(".userfirstname").html(user_firstname);
                 } else {
-                    alert(JSON.stringify(data));
-                    if (data.code == 400) {
-                        window.location.href = "login.html?message=Please Login Again.";
-                    }
+                    $(".userfirstname").html("");
                 }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                // set [submit] button able if the submit is not
-                // successful
+                if (user_lastname != "null") {
+                    $(".userlastname").html(user_lastname);
+                } else {
+                    $(".userlastname").html("");
+                }
+                $(".useremail").html(user_email);
+                checkin_afterChencin();
+            } else {
+                alert(JSON.stringify(data));
+
+                window.location.href = "login.html?message=Please Login Again.";
+
             }
-        });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            // set [submit] button able if the submit is not
+            // successful
+            window.location.href = "login.html?message=Please Login Again."
+        }
+    });
 }
 
 $(function () {
@@ -201,7 +202,7 @@ $(function () {
                     window.location.href = "login.html?message=Logout success.";
                 } else {
                     alert(JSON.stringify(data));
-                     //清除cookie
+                    //清除cookie
                     $.removeCookie('token', {
                         path: '/'
                     });
@@ -225,24 +226,24 @@ $(function () {
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
-//                $.removeCookie('token', {
-//                    path: '/'
-//                });
-//                $.removeCookie('email', {
-//                    path: '/'
-//                });
-//                $.removeCookie('firstname', {
-//                    path: '/'
-//                });
-//                $.removeCookie('lastname', {
-//                    path: '/'
-//                });
-//                $.removeCookie('user_picture', {
-//                    path: '/'
-//                });
-//                $.removeCookie('user_id', {
-//                    path: '/'
-//                });
+                $.removeCookie('token', {
+                    path: '/'
+                });
+                $.removeCookie('email', {
+                    path: '/'
+                });
+                $.removeCookie('firstname', {
+                    path: '/'
+                });
+                $.removeCookie('lastname', {
+                    path: '/'
+                });
+                $.removeCookie('user_picture', {
+                    path: '/'
+                });
+                $.removeCookie('user_id', {
+                    path: '/'
+                });
                 window.location.href = "login.html?message=Please Login Again..";
             }
         });
